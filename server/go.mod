8e91590f3772526c07173dd2b2fe43c842149c78
politewang <politewang@tencent.com>
2022-06-11 20:24:14 +0800

    markdown to html

--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/gofrs/uuid v4.2.0+incompatible
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/yaml.v2 v2.2.8
--- server/middleware/email.go
+++ server/middleware/email.go
@@ -1,12 +1,25 @@
 package middleware
 
 import (
+	"bytes"
 	"log"
 
 	"github.com/binbin6363/web-project/server/config"
 	"github.com/binbin6363/web-project/server/model"
+	blackfriday "github.com/russross/blackfriday/v2"
 	"gopkg.in/gomail.v2"
 )
+
+func markDown2Html(markDown string) string {
+	tmpData := bytes.Replace([]byte(markDown), []byte("\r"), nil, -1)
+
+	htmlData := string(blackfriday.Run(
+		tmpData,
+		blackfriday.WithExtensions(blackfriday.CommonExtensions|blackfriday.HardLineBreak),
+	))
+
+	return htmlData
+}
 
 // EmailSend 发送email
 func EmailSend(info *model.EmailInfo) error {
@@ -25,7 +38,8 @@ func EmailSend(info *model.EmailInfo) error {
 	//主题
 	m.SetHeader("Subject", info.Subject)
 	//内容
-	m.SetBody("text/html", info.Body)
+	htmlBody := markDown2Html(info.Body)
+	m.SetBody("text/html", htmlBody)
 	//附件
 	//m.Attach("./myIpPic.png")
 
@@ -34,7 +48,7 @@ func EmailSend(info *model.EmailInfo) error {
 		config.GetConfig().Email.From, config.GetConfig().Email.Token)
 
 	// 发送邮件
-	err := d.DialAndSend(m)
+	err = d.DialAndSend(m)
 	if err != nil {
 		log.Printf("DialAndSend err %v:", err)
 		return err
