fee7ddf67b20c182fff9a04f3f7aedf4c2bac9da
politewang <politewang@tencent.com>
2022-06-12 20:18:02 +0800

    format log

--- server/route/alerts.go
+++ server/route/alerts.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -25,16 +26,14 @@ func State2Action(state string) string {
 func AlertsHandler(c *gin.Context) {
 	req := new(model.AlertRequest)
 	if err := c.ShouldBindJSON(req); err != nil {
-		fmt.Printf("parse req failed, err:%v\n", err)
+		fmt.Printf("[alert] parse req failed, err=[%v]\n", err)
 		c.JSON(200, gin.H{
 			"err": err.Error(),
 		})
 		return
 	}
 
-	fmt.Printf("recv: %v\n", req)
 	meta := fmt.Sprintf("match count:%d", len(req.EvalMatches))
-
 	smsReq := &model.SmsAlertReq{
 		Ip:           req.RuleName,
 		SourceTime:   time.Now().Format("2006-01-02 15:04:05"),
@@ -46,11 +45,9 @@ func AlertsHandler(c *gin.Context) {
 		Action:       State2Action(req.State),
 	}
 
-	fmt.Printf("smsReq: %v, alert cfg:%v\n", smsReq, config.GetConfig().Alert)
 	body, _ := json.Marshal(smsReq)
 	reqHttp, err := http.NewRequest("POST", config.GetConfig().Alert.Addr, bytes.NewBuffer(body))
 	if err != nil {
-		fmt.Printf("make sms req fail, err:%v, body:%s\n", err, string(body))
 		c.JSON(200, gin.H{
 			"err": err.Error(),
 		})
@@ -58,19 +55,20 @@ func AlertsHandler(c *gin.Context) {
 	reqHttp.Header.Set("Content-Type", "application/json")
 	reqHttp.Header.Set("X-Secret", config.GetConfig().Alert.Secret)
 
-	fmt.Printf("after Set Header:%v, body:%s\n", reqHttp.Header, string(body))
 	client := &http.Client{}
 	resp, err := client.Do(reqHttp)
 	if err != nil {
-		fmt.Printf("req sms fail, err:%v, req:%v\n", err, reqHttp)
+		fmt.Printf("[alert] request sms service fail, err=[%v], req=[%v]\n", err, reqHttp)
 		c.JSON(200, gin.H{
 			"err": err.Error(),
 		})
 		return
 	}
 	defer resp.Body.Close()
-	fmt.Printf("after client.Do:%v, body:%s\n", reqHttp.Header, string(body))
 	c.JSON(200, gin.H{
 		"err": "ok",
 	})
+
+	rspBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Printf("[alert] ok send req=[%s], rsp=[%s]\n", string(body), string(rspBody))
 }
